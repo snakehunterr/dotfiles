37c7fe182f535adffb624439e2bebc12b7bed43b
snakehunterr <https://github.com/snakehunterr>
2025-05-22 15:11:32 +0300

    🔧 Updated fish, hyprland, kitty, btop

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,8 +1,8 @@
 function fish_title
     set -l curr (status current-command)
-    if test $curr != fish
-        echo \$ $curr
+    if test $curr = fish
+        prompt_pwd
     else
-        echo (basename $PWD)
+        echo (string shorten --max 15 (status current-commandline))
     end
 end
