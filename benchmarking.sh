bd54ad1ea802257b5222056ced54fc2025c260f9
snakehunterr <https://github.com/snakehunterr>
2025-05-22 15:23:51 +0300

    🔥 Remove unnecessary files

--- benchmarking.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# Single thread
-time cat </dev/urandom | head -c 1G | gzip >/dev/null
-
-# Multi thread
-time cat </dev/urandom | head -c 1G | pigz >/dev/null
--- power_management.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-sudo pacman -S --needed --noconfirm tlp tlp-rdw
-sudo systemctl enable tlp.service
-sudo systemctl enable NetworkManager-dispatcher.service
-sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
